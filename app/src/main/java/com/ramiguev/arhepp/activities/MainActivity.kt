55db6367b37840219b7488e3976c78ba996b60cf
Juan <juang0978@gmail.com>
2021-09-25 22:19:50 -0300

    #2 Interfaz de HomeActivity

--- app/src/main/java/com/ramiguev/arhepp/activities/MainActivity.kt
+++ app/src/main/java/com/ramiguev/arhepp/activities/MainActivity.kt
@@ -2,11 +2,15 @@ package com.ramiguev.arhepp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.ramiguev.arhepp.R
+import com.ramiguev.arhepp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
