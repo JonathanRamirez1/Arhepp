9ee32e7af17a2e5a9f8dd84c6a14c4e27f17a3e4
Juan <juang0978@gmail.com>
2021-09-27 20:10:01 -0300

    Integracion de Google Apiclient

--- app/src/main/java/com/ramiguev/arhepp/fragments/LoginFragment.kt
+++ app/src/main/java/com/ramiguev/arhepp/fragments/LoginFragment.kt
@@ -5,15 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.ramiguev.arhepp.R
 import com.ramiguev.arhepp.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), GoogleApiClient.OnConnectionFailedListener {
 
     private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun getGoogleApiClient() : GoogleApiClient? {
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        return context?.let {
+            GoogleApiClient.Builder(it)
+                .enableAutoManage(FragmentActivity(), this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build()
+        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Toast.makeText(context, "Connection Failed!!", Toast.LENGTH_SHORT).show()
     }
 }
